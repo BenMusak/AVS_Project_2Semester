149cb3537ff99ea69445377502d40cab0970f384
BenMusak <56428534+BenMusak@users.noreply.github.com>
2023-03-30 14:50:05 +0200

    Initial commit
    First Commit

---
+++ LDA.py
@@ -0,0 +1,103 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.colors as mcolors
+
+
+class LDA:
+    def __init__(self, n_components):
+        self.n_components = n_components
+        self.linear_discriminants = None
+        
+    def fit(self, data, labels):
+        # First we get the features and labels. The function unique returns the unique values in the array, so that we have the different labels without repetitions.
+        n_features = data.shape[1]
+        class_labels = np.unique(labels)
+        
+        # Get the mean of the dataset
+        mean_overall = np.mean(data, axis=0)
+        # Initialize S_W and S_B with the size of the number of features
+        SW = np.zeros((n_features, n_features)) # 784, 784
+        SB = np.zeros((n_features, n_features)) # 784, 784
+        
+        # Fishers LDA for multiple classes
+        for c in class_labels:
+            d = labels == c
+            X_c = []
+            for i in range(len(d)):
+                if d[i] == True:
+                    X_c.append(data[i])
+                else:
+                    pass
+            
+            # Turn it into an array from a list
+            X_c = np.array(X_c)
+            
+            # Get the mean of the current class
+            mean_c = np.mean(X_c, axis=0)
+            
+            # Within-class covariance matrix, (n, n_c) * (n_c, n) = (n, n)
+            SW += (X_c - mean_c).T.dot((X_c - mean_c))
+            
+            # Between-class covariance matrix
+            n_c = X_c.shape[0]
+            mean_diff = (mean_c - mean_overall).reshape(n_features, 1)
+            SB += n_c * (mean_diff).dot(mean_diff.T)
+
+        # Finding SW^-1 * SB. Since the determinant of SW and SB is 0 we can't use inv, but we can use pinv.
+        W = np.linalg.pinv(SW).dot(SB)
+
+        # Get eigenvalues and eigenvectors of A
+        eigenvalues, eigenvectors = np.linalg.eig(W)
+
+        # sort eigenvalues high to low
+        eigenvectors = eigenvectors.T
+        idxs = np.argsort(abs(eigenvalues))[::-1]
+        eigenvalues = eigenvalues[idxs]
+        eigenvectors = eigenvectors[idxs]
+
+        # store first n eigenvectors in linear_discriminants since it is the biggest
+        self.linear_discriminants = eigenvectors[0 : self.n_components]
+    
+    def transform(self, X):
+        # project data onto linear discriminants
+        return np.dot(X, self.linear_discriminants.T)
+
+
+def plot_data(train_, trn_targets, title):
+
+    color = ["red", "green", "blue"]
+    labels = ["SG", "SC", "LP"]
+    for i in range(len(train_)):
+        if trn_targets[i] == 1:
+            plt.scatter(train_[i][0], train_[i][1], color=color[0], edgecolors="none", label=labels[0], alpha=0.8)
+        elif trn_targets[i] == 2:
+            plt.scatter(train_[i][0], train_[i][1], color=color[1], edgecolors="none", label=labels[1], alpha=0.8)
+        else:
+            plt.scatter(train_[i][0], train_[i][1], color=color[2], edgecolors="none", label=labels[2], alpha=0.8)
+
+    plt.suptitle(title)
+    plt.xlabel("Linear Discriminant 1")
+    plt.ylabel("Linear Discriminant 2")
+
+    # Create a custom legend with the same colors as the corresponding class
+    handles = [plt.Rectangle((0,0),1,1, color=color[i], alpha=0.8) for i in range(len(labels))]
+    plt.legend(handles, labels)
+    plt.show()
+
+
+def LDA_Fishers(trainset, trn_targets, test, n_components):
+    lda = LDA(n_components)
+    lda.fit(trainset, trn_targets)
+
+    train_ =[]
+    train_tgt = []
+    test_ = []
+    for i in range(len(trainset)):
+        train_.append(lda.transform(trainset[i]))
+        train_tgt.append(trn_targets[i])
+    for i in range(len(test)):
+        test_.append(lda.transform(test[i][0]))
+    if n_components == 2:
+        plot_data(train_, train_tgt, "LDA Fisher's")
+    
+    return train_, test_
---
+++ audio_lib.py
@@ -0,0 +1,145 @@
+import librosa
+import IPython.display as ipd
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+import numpy as np
+
+
+# Function to load audio file
+def load_audio_file(file_path):
+    print("Loading Audio File...")
+    signal, sr = librosa.load(file_path)
+    return signal, sr
+
+
+def play_audio_file(y, sr):
+    ipd.Audio(y, rate=sr)
+
+
+def get_samples(signal):
+    print(signal.shape)
+    return signal.shape[0]
+
+
+def extract_MFCCs(y, sr, res=11):
+    print("Extracting MFCCs...")
+    MFCCs = librosa.feature.mfcc(y=y, n_mfcc=res, sr=sr)
+    print("MFCCs shape: ", MFCCs.shape)
+    return MFCCs
+
+
+def extract_Mel(y, sr, res=128):
+    print("Extracting Mel...")
+    S = librosa.feature.melspectrogram(y=y, sr=sr, n_mels=res)
+    print("Mel shape: ", S.shape)
+    return S
+
+
+def mean_mfccs(x):
+    return [np.mean(feature) for feature in librosa.feature.mfcc(x)]
+
+
+def visualize_MFCCs_Mel(MFCCs, Mel, sr):
+    print("Visualizing MFCCs...")
+    fig, ax = plt.subplots(nrows=2, sharex=True)
+    img_mel = librosa.display.specshow(librosa.power_to_db(Mel, ref=np.max),
+                               x_axis='time', y_axis='mel', fmax=8000,
+                               ax=ax[0])
+    fig.colorbar(img_mel, ax=[ax[0]])
+    ax[0].set(title='Mel spectrogram')
+    ax[0].label_outer()
+    img_MFCCs = librosa.display.specshow(MFCCs, x_axis='time', sr=sr)
+    fig.colorbar(img_MFCCs, ax=[ax[1]])
+    ax[1].set(title='MFCC')
+    plt.title('MFCCs')
+    plt.show()
+
+
+
+def dataset_split(MFCCs, Mel, sr, label, res=100, diff_extremes=43, display=False):
+
+    print("Splitting dataset...")
+
+    print("Finding non-silent parts...")
+    # Find places in the MFCCs where the audio is silent looking at the first coefficient
+    non_silent_parts = np.where(MFCCs[0, :] > -500)[0]
+    print(non_silent_parts)
+
+    # For Each new non-silent part after a silent part, merge the next 50 non-silent parts
+    # into the current non-silent part. Then remove the next non-silent parts until a new silent part is meet.
+    # Remember to check for out of bounds
+    i = 0
+    strum_single = []
+    strums_list = []
+    new_strum = True
+    check_index = []
+    while i < len(non_silent_parts) - 1:
+        diff = abs(MFCCs[0, non_silent_parts[i]] - MFCCs[0, non_silent_parts[i - 1]])
+        #print(diff)
+        if diff > diff_extremes and new_strum:
+            if non_silent_parts[i] + 1 == non_silent_parts[i + 1]:
+                for j in range(0, res):
+                    strum_single.append(MFCCs[:, non_silent_parts[i + j]])
+                    check_index.append(non_silent_parts[i + j])
+                    try:
+                        if MFCCs[:, i + j].all() == MFCCs[:, non_silent_parts[i]].all():
+                            non_silent_parts = np.delete(non_silent_parts, np.arange(i, i))
+                    except IndexError:
+                        pass
+                strums_list.append(strum_single)
+                strum_single = []
+                new_strum = False
+        else:
+            new_strum = True
+        i += 1
+
+    # Convert the list of strums to a numpy array
+    strums_list = np.asarray(strums_list)
+
+    # Sum the MFCCs of each strum to get a single value for each strum
+    strums_list = np.sum(strums_list, axis=1)
+
+    print(strums_list.shape)
+
+    # Give the strums a label
+    labeled_parts = []
+    for i in range(0, len(strums_list)):
+        labeled_parts.append((strums_list[i], label))
+
+
+    #print("Non-silent parts:" + str(len(non_silent_parts)))
+    print("Strums:" + str(len(strums_list)))
+    #print("One strum:" + str(strums_list[0]))
+    print("All parts:" + str(len(MFCCs[1])))
+
+    # Visualize the non-silent parts
+    if display:
+        visualize_MFCCs_Mel(MFCCs[:, check_index], Mel[:, check_index], sr)
+
+    # Split the labeled parts into training and test sets
+    train, test = train_test_split(labeled_parts, test_size=0.2)
+
+    # Split the training set into training and validation sets
+    #train, val = train_test_split(train, test_size=0.2)
+
+    # Extract the MFCCs and labels from the training, validation, and test sets
+    train_x, train_y = zip(*train)
+    #val_x, val_y = zip(*val)
+    test_x, test_y = zip(*test)
+
+    # Convert the MFCCs to numpy arrays
+    train_x = np.array(train_x)
+    #val_x = np.array(val_x)
+    test_x = np.array(test_x)
+
+    # Convert the labels to numpy arrays
+    train_y = np.array(train_y)
+    #val_y = np.array(val_y)
+    test_y = np.array(test_y)
+
+    return train_x, train_y, test_x, test_y
+
+
+def save_dataset(train_x, train_y, test_x, test_y):
+    print("Saving dataset...")
+    np.savez("dataset.npz", train_x=train_x, train_y=train_y, test_x=test_x, test_y=test_y)
