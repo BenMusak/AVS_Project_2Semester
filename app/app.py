09fb69b3208f5ffa60cd282e8947231efe6b4484
Kata Bujdosó <86352493+katab99@users.noreply.github.com>
2023-04-26 09:44:38 +0200

    Update

--- app/app.py
+++ app/app.py
@@ -1,68 +1,76 @@
 import streamlit as st
 import dataset as ds
-import model as m
+# import model as m
 import record as r
+import convert as c
+#import utils 
 
 header = st.container() # just title
 record = st.container() # record audio and show audio wave
-knn_clf = st.container() # results of classification
+clf = st.container() # results of classification
 
 CLASSIFY = False # doing classification
 
 #################################
-# TODO : if possible make this part somehow visible, e.g. loading bar
-
 # load dataset
 label_guitar_model = {
-    'LP' : 0,
-    'SC' : 1, 
-    'SG' : 2,
-    'TC' : 3
+    0 : 'LP',
+    1 : 'SC', 
+    2 : 'SG',
+    3 : 'TC'
     }
 
-dirs = [
-    r'C:\Users\Kata\OneDrive - Aalborg Universitet\CE8-DESKTOP-T44IC5T\Project\2. Data\1. Sound_samples\5. Full_recordings\All_Collected\Guitar_Models\LP', 
-    r'C:\Users\Kata\OneDrive - Aalborg Universitet\CE8-DESKTOP-T44IC5T\Project\2. Data\1. Sound_samples\5. Full_recordings\All_Collected\Guitar_Models\SC',
-    r'C:\Users\Kata\OneDrive - Aalborg Universitet\CE8-DESKTOP-T44IC5T\Project\2. Data\1. Sound_samples\5. Full_recordings\All_Collected\Guitar_Models\SG', 
-    r'C:\Users\Kata\OneDrive - Aalborg Universitet\CE8-DESKTOP-T44IC5T\Project\2. Data\1. Sound_samples\5. Full_recordings\All_Collected\Guitar_Models\TC'
-    ]
+#dirs = [
+#    r'C:\Users\Kata\OneDrive - Aalborg Universitet\CE8-DESKTOP-T44IC5T\Project\2. Data\1. Sound_samples\5. Full_recordings\All_Collected\Guitar_Models\LP', 
+#    r'C:\Users\Kata\OneDrive - Aalborg Universitet\CE8-DESKTOP-T44IC5T\Project\2. Data\1. Sound_samples\5. Full_recordings\All_Collected\Guitar_Models\SC',
+#    r'C:\Users\Kata\OneDrive - Aalborg Universitet\CE8-DESKTOP-T44IC5T\Project\2. Data\1. Sound_samples\5. Full_recordings\All_Collected\Guitar_Models\SG', 
+#    r'C:\Users\Kata\OneDrive - Aalborg Universitet\CE8-DESKTOP-T44IC5T\Project\2. Data\1. Sound_samples\5. Full_recordings\All_Collected\Guitar_Models\TC'
+#    ]
 
-audio= ds.Dataset()
-audio.create_dataset(dirs)
-audio.scale() # scale dataset
+#audio= ds.Dataset()
+#audio.create_dataset(dirs)
+#audio.scale() # scale dataset
 
 
 # model and training
-model = m.KNN()
-model.train(audio.X_train, audio.y_train)
+#model = m.KNN()
+#model.train(audio.X_train, audio.y_train)
+#FILE_PATH = ""
+#model = utils.load_model(FILE_PATH)
 
 #################################
 
 with header:
-    header.title('Guitar Classification 🎸')
+    st.title('Guitar Classification 🎸')
 
 with record:
-    record.title('Record audio') # TODO : remove later
+    st.title('Record audio') # TODO : remove later
     
-    rec_button = record.button('Record', key='audio-rec')
-    record.write(rec_button) # TODO : comment out in the end
-    #record.line_chart(data)
+    rec_button = st.button('Record', key='audio-rec', on_click=r.record_audio())
+    st.write(rec_button) # TODO : comment out in the end
     
+    audio_data, sr = c.load_file()
+    
+    # show linechart if record button was pressed
     if rec_button:
-        data = r.record_audio()
-        record.line_chart(data)
-        rec_button = False
-        CLASSIFY = True
-
+        st.line_chart(audio_data)
+    
 
 # TODO : convert record to classification
+with clf: 
+    st.title('Classification') # TODO : remove later
 
-with knn_clf: 
-    knn_clf.title('kNN Classification') # TODO : remove later
+    clf_button = st.button('Convert', key='knn-clf') # TODO : remove later
+    mfcc_data = c.convert(audio_data, sr)
+    
+    if clf_button:
+        #mfcc_data = c.convert(audio_data, sr)
+        st.write("Data converted")
+        clf_button = False
 
-    if CLASSIFY:
-        CLASSIFY = False
         # make kNN prediction
-
-        y_pred = model.predict(data)
-        pass
+        #y_pred = model.predict(data)
+        #y_label = label_guitar_model[y_pred]
+#        st.metric(label="Class", value=y_label)
+#        CLASSIFY = False
+#        pass
