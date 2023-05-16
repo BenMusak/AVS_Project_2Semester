6e678680bc25bd77c3748871f69b55767be7d485
vmatth <vmatth19@student.aau.dk>
2023-05-16 11:44:06 +0200

    Added new CNN model

--- app/pytorch_utils.py
+++ app/pytorch_utils.py
@@ -7,61 +7,88 @@ class CNNClassifier(pl.LightningModule):
         super(CNNClassifier, self).__init__()
 
         #In channels, Out channels
-        self.conv1 = nn.Conv2d(1, 16, kernel_size=3, stride=1, padding=1)
-        self.conv2 = nn.Conv2d(16, 32, kernel_size=3, stride=1, padding=1)
-        self.conv3 = nn.Conv2d(32, 64, kernel_size=3, stride=1, padding=1)
-        self.conv4 = nn.Conv2d(64, 128, kernel_size=3, stride=1, padding=1)
-        
+        self.conv1 = nn.Conv2d(1, 32, kernel_size=3, stride=1, padding=0)  
+        self.conv2 = nn.Conv2d(32, 64, kernel_size=3, stride=1, padding=0)
+        self.conv3 = nn.Conv2d(64, 64, kernel_size=3, stride=1, padding=0)
+
         self.maxpool = nn.MaxPool2d(kernel_size=2)
         
-        self.fc1 = nn.Linear(128 * 8 * 11, 256)
+        self.fc1 = nn.Linear(64 * 61 * 91, 128)
+        self.fc2 = nn.Linear(128, 126)
 
-        self.manufacturer = nn.Linear(256, 7)
-        self.guitar_type = nn.Linear(256, 4)
-        self.pickup = nn.Linear(256, 2)
-        self.pickup_position = nn.Linear(256, 3)
-        self.strumming = nn.Linear(256, 2)
-        self.player = nn.Linear(256, 5)
+        self.manufacturer = nn.Linear(126, 7)
+        self.guitar_type = nn.Linear(126, 4)
+        self.pickup = nn.Linear(126, 2)
+        self.pickup_position = nn.Linear(126, 3)
+        self.strumming = nn.Linear(126, 2)
+        self.player = nn.Linear(126, 6)
         
         self.relu = nn.ReLU()
+        self.softmax = nn.Softmax(dim=1)
+
+        self.criterion = nn.CrossEntropyLoss()
+        
+        self.confmat_manufacturer = torchmetrics.ConfusionMatrix(num_classes = 7, task='multiclass') 
+        self.confmat_guitar_type = torchmetrics.ConfusionMatrix(num_classes = 4, task='multiclass')
+        self.confmat_pickup = torchmetrics.ConfusionMatrix(num_classes = 2, task='multiclass')
+        self.confmat_pickup_position = torchmetrics.ConfusionMatrix(num_classes = 3, task='multiclass')
+        self.confmat_strumming = torchmetrics.ConfusionMatrix(num_classes = 2, task='multiclass')
+        self.confmat_player = torchmetrics.ConfusionMatrix(num_classes = 6, task='multiclass')
+        
+        self.confusion_matrix_manufacturer = np.zeros((7, 7))
+        self.confusion_matrix_guitar_type = np.zeros((4,4))
+        self.confusion_matrix_pickup = np.zeros((2,2))
+        self.confusion_matrix_pickup_position = np.zeros((3,3))
+        self.confusion_matrix_strumming = np.zeros((2,2))
+        self.confusion_matrix_player = np.zeros((6,6))
+
+        self.test_loss = []
+        self.test_acc_manufacturer = []
+        self.test_acc_guitar = []
+        self.test_acc_pickup = []
+        self.test_acc_pickup_position = []
+        self.test_acc_strumming = []
+        self.test_acc_player = []
         
     def forward(self, x):
         x = self.relu(self.conv1(x))
-        x = self.maxpool(x)
         x = self.relu(self.conv2(x))
-        x = self.maxpool(x)
         x = self.relu(self.conv3(x))
         x = self.maxpool(x)
-        x = self.relu(self.conv4(x))
-        x = self.maxpool(x)
 
-        x = x.view(-1, 128 * 8 * 11)        
+        x = x.view(-1, 64 * 61 * 91)        
         x = self.relu(self.fc1(x))
-
+        x = x.view(-1, 128)  
+        x = self.fc2(x)
         return { 
-            'manufacturer': self.manufacturer(x),
-            'guitar_type': self.guitar_type(x),
-            'pickup':self.pickup(x),
-            'pickup_position': self.pickup_position(x),
-            'strumming': self.strumming(x),
-            'player': self.player(x)
+            'manufacturer': self.softmax(self.manufacturer(x)),
+            'guitar_type': self.softmax(self.guitar_type(x)),
+            'pickup':self.softmax(self.pickup(x)),
+            'pickup_position': self.softmax(self.pickup_position(x)),
+            'strumming': self.softmax(self.strumming(x)),
+            'player': self.softmax(self.player(x))
         }
 
+    def multilabel_loss_fn(self, y_hats, ys):
+        losses=0
+        for i, key in enumerate(y_hats):
+            losses += self.criterion(y_hats[key], ys[key])
+        return losses
 
-def multilabel_predictions(y_hats):
-    predictions=[]
-    for i, key in enumerate(y_hats):
-        _, predicted=torch.max(y_hats[key], 1)
-        predictions.append(predicted)
-    return predictions
+    def multilabel_predictions(self, y_hats, ys):
+        predictions=[]
+        for i, key in enumerate(y_hats):
+            _, predicted=torch.max(y_hats[key], 1)
+            predictions.append(predicted)
+        return predictions
 
-def multilabel_accuracy(predictions, ys):
-    accuracies=[]
-    ys = list(ys.values()) #Dictionary to list
-    for idx, predicted in enumerate(predictions):
-        accuracy = (predicted == ys[idx]).sum().item() / len(predicted)
-        accuracies.append(accuracy)
-    return accuracies
+    def multilabel_accuracy(self, predictions, ys):
+        accuracies=[]
+        ys = list(ys.values()) #Dictionary to list
+        for idx, predicted in enumerate(predictions):
+            accuracy = (predicted == ys[idx]).sum().item() / len(predicted)
+            accuracies.append(accuracy)
+        return accuracies
 
 #########Labels########
 def get_manufacturer_labels():
